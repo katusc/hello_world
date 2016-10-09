29210eadcd36587848d3f9369dbeb2f590b9d75c
katuska <katerina.caletkova@gmail.com>
2016-10-09 22:45:10 +0200

    add new hello world projekt

---
+++ my-app/src/main/java/com/hello_world/app/App.java
@@ -0,0 +1,13 @@
+package com.hello_world.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello PA165!" );
+    }
+}
